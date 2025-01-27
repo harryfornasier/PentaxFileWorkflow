d5be3238fcaf0f5440f6ea7341aa55c2a6a54426
Harrison Fornasier <harrisonfornasier@gmail.com>
2025-01-27 14:02:42 +0000

    Add ability to move files from SD to Photos and create folder

--- name_file.py
+++ name_file.py
@@ -1,32 +1,52 @@
 from pathlib import Path
+import shutil
+from osxmetadata import OSXMetaData
 import os
-
 
 #These are the extensions for the Pentax camera.
 #Change them if you want to use this script with a different raw file.
 target_extensions = [".pef", ".xmp"]
 
+def check_comments(directory):
+    folders_without_comments = []
 
-def set_comment(file_path, comment_text):
-    import applescript
-    applescript.tell.app("Finder", f'set comment of (POSIX file "{file_path}" as alias) to "{comment_text}" as Unicode text')
+    for root, dirs, files in os.walk(directory):
+        for name in dirs:
+            path = os.path.join(root, name)
+            md = OSXMetaData(path)
+            if not md.findercomment:
+                folders_without_comments.append(name)
 
-folder_name = input("What would you like the folder to be called? This name will be used for RAW, OOC jpeg, and and your output folder: ")
-root_path = os.path.expanduser("~/Pictures/") + folder_name
-os.mkdir(root_path)
-set_comment(root_path, 'test')
-
-raw_name = folder_name + "_raw"
-folder_path = Path(raw_name)
-folder_path.mkdir(exist_ok=True)
-
-print(f"Folder '{folder_name}' has been created.")
+    if folders_without_comments:
+        return folders_without_comments
+    else:
+        return False
 
 
-for file in Path.cwd().iterdir():
-    if file.is_file():
-        ext = file.suffix.lower()
-        if ext in target_extensions:
-            dest = folder_path / file.name
-            file.rename(dest)
-            print(f"Moved: {file.name} ➔ {folder_name}/")
+new_folders = check_comments("./DCIM")
+
+if new_folders:
+    for folder in new_folders:
+        folder_name = input(f"What would you like the folder {folder} to be called? This name will be used for RAW, OOC jpeg, and and your output folder: ")
+        root_path = Path(os.path.expanduser(f"~/Pictures/{folder_name}"))
+        os.mkdir(root_path)
+
+        folder_path = Path(f"{root_path}/{folder_name}_raw")
+        folder_path.mkdir(exist_ok=True)
+
+        print(f"Folder '{folder_name}' has been created.")
+
+
+        for file in Path(f"./DCIM/{folder}").iterdir():
+            if file.is_file():
+                ext = file.suffix.lower()
+                if ext in target_extensions:
+                    dest = folder_path / file.name
+                    shutil.copy2(file, dest)
+                    print(f"Copied: {file.name} ➔ {folder_name}/")
+                else:
+                    dest = root_path / file.name
+                    shutil.copy2(file, dest)
+                    print(f"Copied: {file.name} ➔ {folder_name}/")
+else:
+    print("There seem to be no new folders")
